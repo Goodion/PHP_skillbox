7e85cfab82117672565fd249482f9b2d4e387a0e
Ivan Saltykov <hcys@list.ru>
2020-01-20 20:43:45 +0300

    homework 4_3 initial commit

---
+++ homework4_3/creator/index.php
@@ -0,0 +1,57 @@
+<?php
+
+abstract class Item
+{
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function show()
+    {
+        echo('<br />');
+        echo('Я ' . $this->name . PHP_EOL);
+        echo('<br />');
+    }
+}
+
+class EmptyItem extends Item
+{
+    public function show()
+    {
+        echo('<br />');
+        echo('Класс ' . $this->name . ' не найден' . PHP_EOL);
+        echo('<br />');
+    }
+}
+
+class Creator
+{
+    public static function create($name)
+    {
+        $classes = get_declared_classes();
+        if (in_array($name, $classes)) {
+            return new $name($name);
+        } else {
+            return new EmptyItem($name);
+        }
+    }
+}
+
+class Book extends Item{};
+class Cat extends Item{};
+class Bird extends Item{};
+class House extends Item{};
+class Table extends Item{};
+class Pen extends Item{};
+class Nose extends Item{};
+class Box extends Item{};
+class Robot extends Item{};
+
+$creator = new Creator;
+$arr = ['Book', 'asdqv', 'Nose', 'Cat', 'Idwqsw', 'dsqq', 'Rasdf', 'House', 'Dqweq', 'Pen', 'Robot'];
+
+foreach ($arr as $item) {
+    $newObj = $creator::create($item);
+    $newObj->show();
+}
---
+++ homework4_3/formatter/index.php
@@ -0,0 +1,76 @@
+<?php
+
+interface Renderable
+{
+    public function render($string);
+}
+
+interface Formatter
+{
+    public function format($string);
+}
+
+class Display
+{
+    public static function show($formatter, $string)
+    {
+        if ($formatter instanceof Renderable) {
+            echo($formatter->render($string));
+        } else if ($formatter instanceof Formatter || method_exists($formatter, 'format')) {
+            echo($formatter->format($string));
+        }
+    }
+}
+
+class Tag implements Renderable
+{
+    public function render($string)
+    {
+        $arr = explode('@@', $string);
+        if (count($arr) === 2) {
+            echo('<' . $arr[0] . '>' . $arr[1] . '</' . $arr[0] . '>');
+        } else {
+            echo($arr[0]);
+        }
+        
+    }
+}
+
+class Str implements Formatter
+{
+    public function format($string)
+    {
+        $string = mb_strtoupper($string);
+        echo($string);
+    }
+}
+
+class Num 
+{
+    public function format($string)
+    {
+        $result = iconv_strlen($string);
+        echo($result);
+    }
+}
+
+$arr = [
+    'h2@@Lorem ipsum dolor sit amet consectetur',
+    'h1@@adipisicing elit. Illo suscipit',
+    'h3@@adipisicing elit. Illo suscipit quam aperiam',
+    'adipisicing elit. Illo suscipit quam aperiam',
+    'adipisicing elit. Illo suscipit quam aperiam',
+    'Illo suscipit quam aperiam quibusdam',
+    'iusto nulla, dolorem in aut. Mollitia',
+    'adipisicing elit.   dolore quod voluptas dolorum autem',
+    ' , ipsam iure explicabo! Autem, mollitia recusandae!',
+];
+
+foreach ($arr as $item) {
+    echo ('<br />');
+    Display::show(new Tag, $item);
+    echo ('<br />');
+    Display::show(new Str, $item);
+    echo ('<br />');
+    Display::show(new Num, $item);
+}
